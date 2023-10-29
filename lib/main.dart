792948e2f470b58d343f3c299530a7668dc606f5
Kishna Bansal <kb.20051981@gmail.com>
2023-10-29 16:06:35 +0530

    day 3 completed

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
-import 'package:catalog_app/home_page.dart';
+import 'package:catalog_app/pages/home_page.dart';
 import 'package:flutter/material.dart';
+import 'package:catalog_app/pages/login_page.dart';
 
 void main() {
   runApp(const MyApp());
@@ -23,8 +24,18 @@ class MyApp extends StatelessWidget {
     //const pi=3.14;
     //final 
 
-    return const MaterialApp(
-      home: HomePage(),
+    return MaterialApp(
+      themeMode: ThemeMode.light,
+      theme: ThemeData(
+        primarySwatch: Colors.blueGrey,
+        brightness: Brightness.light
+      ),
+      //initialRoute: "/home",
+      routes: {
+        "/":(context) => const LoginPage(),      //default route
+        "/home":(context) => const HomePage(),
+        "/login":(context) => LoginPage()
+      },
     );
   }
 }
